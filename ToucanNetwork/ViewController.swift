46b315a366b7553daf8d732fdda5206cf2d16e97
bing wang <icewong401@163.com>
2018-09-21 16:30:03 +0800

    Initial Commit

---
+++ ToucanNetwork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToucanNetwork
+//
+//  Created by bing wang on 21/9/18.
+//  Copyright © 2018年 bing wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
